48441c3cbe25db34d277bf00288475a77fbd64b4
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-28 00:16:51 +0000

    Create function that compares if two values it takes in matches or not

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Passed: 🛑🛑🛑 ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, '1');
+assertEqual("Hello", "hello");
+assertEqual("Harry Potter", "Harry Potter");
