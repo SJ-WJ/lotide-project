19640c419ab74986e4bdffa7fef62dea8b0477ff
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-29 01:55:07 +0000

    Add square brackets inside the template literal

--- head.js
+++ head.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ✅✅✅ ${actual} === ${expected}`);
+    console.log(`Assertion Passed: ✅✅✅ ${[actual]} === ${[expected]}`);
   } else {
-    console.log(`Assertion Passed: 🛑🛑🛑 ${actual} !== ${expected}`);
+    console.log(`Assertion Passed: 🛑🛑🛑 ${[actual]} !== ${[expected]}`);
   }
 
 };
