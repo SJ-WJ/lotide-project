da06b834a6147c8e2490c03f5c8bc4f3073df394
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-30 02:17:57 +0000

    Initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ✅✅✅ ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`Assertion Passed: 🛑🛑🛑 ${[actual]} !== ${[expected]}`);
+  }
+
+};
+
+const findKeyByValue = function(object,value) {
+  for (const key in object) {
+
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+// TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let arrayCompare = eqArrays(actual, expected);
+
+  if (arrayCompare === true) {
+    console.log(`Assertion Passed: ✅✅✅ ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`Assertion Passed: 🛑🛑🛑 ${[actual]} !== ${[expected]}`);
+  }
+
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+
+    if (sentence[i] in results) {
+      results[sentence[i]].push(i);
+      console.log(sentence[i] in results);
+
+    } else {
+      results[sentence[i]] = [i];
+
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+// TEST CODE
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("goodbye").o, [3]);
