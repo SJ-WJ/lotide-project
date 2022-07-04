4ea9c3e0a4195ebeacde3c80792fbe92735e730e
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-07-04 20:32:10 +0000

    Initial commit

---
+++ map.js
@@ -0,0 +1,39 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected) {
+  let arrayCompare = eqArrays(actual, expected);
+  if (arrayCompare === true) {
+    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const map = function(array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    results.push(callback(item));
+
+  }
+  return results;
+};
+
+
+//TEST CODE
+const words = ["boston", "celtics", "are", "champions"];
+const tvShows = ["Prison Break", "Criminal Minds", "Smart Guy", "Lost"];
+
+const result1 = map(words, word => word[0]);
+console.log(result1);
+
+const result2 = map(tvShows, show => show);
+console.log(result2);
---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+// IMPLEMENTATION FUNCTION
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected) {
+  let arrayCompare = eqArrays(actual, expected);
+  if (arrayCompare === true) {
+    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+
+  let sliceArray = [];
+  for (const value of array) {
+    if (callback(value)) {
+      break;
+
+    }sliceArray.push(value);
+
+  } return sliceArray;
+};
+
+
+// TEST CODE
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
