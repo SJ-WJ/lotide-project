79a8addd0945527cdedb212adba10d5597877fa7
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-07-04 22:28:26 +0000

    Refactor middle function and create middle test file

--- middle.js
+++ middle.js
@@ -1,47 +1,16 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(actual, expected) {
-  let arrayCompare = eqArrays(actual, expected);
-
-  if (arrayCompare === true) {
-    console.log(`Assertion Passed: ✅✅✅ ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`Assertion Passed: 🛑🛑🛑 ${[actual]} !== ${[expected]}`);
-  }
-
-};
-
-const middle = function(array) {
+const middle = (array) => {
   const middleNums = Math.floor(array.length / 2);
-  let middlePart = [];
+  const middlePart = [];
 
   for (let i = 0; i < array.length; i++) {
-
-    if (array.length <= 2) {
-      return middlePart;
-    }
-    if (array.length % 2 === 1) {
-      return array[middleNums];
-    }
-    if (array.length % 2 === 0) {
-      return [array[middleNums - 1], array[middleNums]];
-    }
+    if (array.length <= 2) return middlePart;
+    if (array.length % 2 === 1) return array[middleNums];
+    if (array.length % 2 === 0) return [array[middleNums - 1], array[middleNums]];
   }
 };
 
-
-
-// TEST CODE
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
