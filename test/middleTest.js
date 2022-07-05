8443bae269f87b3f0be6ec791477c24550c77516
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-07-05 02:36:46 +0000

    Refactor tail and middle test files using Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,28 @@
 // TEST CODE
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+describe("#middle", () => {
+  it(`If the length of the array is not greater than 2 it should return empty`, () => {
+    const middle1 = [1];
+    const middle2 = [1, 2];
+    assert.deepEqual(middle(middle1), []);
+    assert.deepEqual(middle(middle2), []);
+  });
+
+  it(`If the length of the array is odd grab the middle element of the array`, () => {
+    const middle1 = [1, 2, 3];
+    const middle2 = [1, 2, 3, 4, 5];
+    assert.deepEqual(middle(middle1), 2);
+    assert.deepEqual(middle(middle2), 3);
+  });
+
+  it(`If the length of the array is even grab the middle most elements of the array`, () => {
+    const middle1 = [1, 2, 3, 4];
+    const middle2 = [1, 2, 3, 4, 5, 6];
+    assert.deepEqual(middle(middle1), [2, 3]);
+    assert.deepEqual(middle(middle2), [3, 4]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,33 @@
 // TEST CODE
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#tail - Case 1", () => {
 
-// Test Case 2: Check the original arrayes
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  const result = ["Hello", "Lighthouse", "Labs"];
+  it(`Should return a length of 2`, () => {
+    assert.strictEqual(tail(result).length, 2);
+  });
+
+  it(`Should the index of 0 for Lighthouse`, () => {
+    assert.strictEqual(tail(result)[0], "Lighthouse");
+  });
+
+  it(`Should the index of 1 for Labs`, () => {
+    assert.strictEqual(tail(result)[1], "Labs");
+  });
+
+});
+
+describe("#tail - Case 2", () => {
+
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it(`The array before the use of tail should have a length of 3`, () => {
+    assert.strictEqual(words.length, 3);
+  });
+
+  it(`After the use of tail it should return a new array with the length of 2`, () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+
+});
